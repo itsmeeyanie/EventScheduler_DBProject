8fa7167b5ae615f08c1ea3134f73fe2829e210e4
Diane <minealoneonly@gmail.com>
2017-12-06 00:57:44 +0800

    [added] funtion.php content

--- function.php
+++ function.php
@@ -2,8 +2,29 @@
 <html>
 <head>
 	<title>Function here</title>
+
+	<!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+	<div class="container text-center col-md-4" style="background-color: #edf2f1;">
+		<div class="panel p-5">
+			<form>
+				Name:
+				<input type="text" name="name"><br><br>
+				Event:
+				<input type="text" name="event"><br><br>
+				Start:
+				<input type="text" name="start"><br><br>
+				End:
+				<input type="text" name="end"><br><br>
 
+			  <br>
+			  <button class="btn btn-primary" type="submit">Add Event</button>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
