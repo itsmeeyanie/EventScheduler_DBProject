26b3749df97b1d83c56afc848f8a46fffb5c6db5
Diane <minealoneonly@gmail.com>
2017-12-09 18:20:23 +0800

    [added] the latest functions

--- calendar.php
+++ calendar.php
@@ -20,7 +20,7 @@ if ($timestamp === false) {
 $today = date('Y-m-j', time());
  
 // For H3 title
-$html_title = date('Y / F', $timestamp);
+$html_title = date('F Y', $timestamp);
  
 // Create prev & next month link     mktime(hour,minute,second,month,day,year)
 $prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
@@ -80,13 +80,20 @@ for ( $day = 1; $day <= $day_count; $day++, $str++) {
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="vendor/bootstrap/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="style.css">
-     
+    
+    <link rel="stylesheet" type="text/css" href="fonts/TheLightFont.ttf">
+
 </head>
 <body>
+
+    <?php 
+        // include("nav.php");
+    ?>
+
     <!-- CALENDAR -->
     <div class="container">
         <div class="panel">
-            <div class="panel-heading text-white p-3" style="background-color: #17a2b8">
+            <div class="panel-heading text-white p-4" style="background-color: #17a2b8">
        
                 <h3 class="text-center">
                     <div class="col-1" style="float: left;">
@@ -100,7 +107,7 @@ for ( $day = 1; $day <= $day_count; $day++, $str++) {
                 </h3>
             </div>
             
-            <table class="table table-border">
+            <table class="table table-bordered">
                 <tr>
                     <thead style="background-color: #f4f4f4;">
                         <th>SUN</th>
--- function.php
+++ function.php
@@ -6,25 +6,82 @@
 	<!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="vendor/bootstrap/css/font-awesome.css" rel="stylesheet">
+    
     <link rel="stylesheet" type="text/css" href="style.css">
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="assets/css/custom-styles.css" rel="stylesheet" />
+
 </head>
 <body>
-	<div class="container text-center col-md-4" style="background-color: #edf2f1;">
-		<div class="panel p-5">
-			<form>
-				Name:
-				<input type="text" name="name"><br><br>
-				Event:
-				<input type="text" name="event"><br><br>
-				Start:
-				<input type="text" name="start"><br><br>
-				End:
-				<input type="text" name="end"><br><br>
+	<!-- /. ROW  -->
+	<div class="pt-5 offset-2">
+		<a href="#" onclick="history.go(-1)">
+			<button class="btn btn-default">Back</button>
+		</a>
+	</div>
 
-			  <br>
-			  <button class="btn btn-primary" type="submit">Add Event</button>
-			</form>
+	<div class="container navbar-default p-5">
+		<div class="col-md-3 offset-2" style="float: right;">
+			<button class="btn btn-primary" data-toggle="modal" data-target="#popUpWindow">+ Add Event</button>
 		</div>
+
+    <div class="modal fade" id="popUpWindow">
+    	<div class="modal-dialog">
+    		<div class="modal-content">
+    			<div class="panel-heading modal-header">
+            		<button type="button" class="btn btn-default close" data-dismiss="modal">&times;</button>
+            		<br>
+            		<form>
+            			
+            		</form>
+                </div>
+    		</div>
+    	</div>
+    </div>
+	
+	    <div class="col-md-10 offset-1">
+	            <div class="panel-body">
+	                <div class="panel-group" id="accordion">
+						<div class="panel">
+	                        <div class="panel-heading p-5" style="background-color: #17a2b8;">
+	                        </div>
+	            
+	                <div class="panel-body">
+	                    <div class="panel panel-default">
+	                     <div class="panel-body">
+	                        <div class="table-responsive">
+	                        	<table class="table table-striped table-bordered table-hover" id="dataTables-example">
+	                            <thead>
+	                                        <tr>
+	                                        	<th width="5%">#</th>
+	                                            <th width="25%">Time</th>
+	                                            <th width="40%">Event</th>
+												<th width="25%">Action</th>
+												
+	                                        </tr>
+	                                    </thead>
+			                            <tbody class="text-center"> 
+			                            	<tr> 
+				                            	<td>1</td>
+				                            	<td>10:45 - 11:30</td>  
+				                            	<td>Meeting!</td>   
+				                            	<td>
+				                            		<input class="btn btn-circle btn-primary" type="button" value="View"></input>
+				                            		<input class="btn btn-circle btn-success" type="button" value="Edit"></input>
+				                            		<input class="btn btn-circle btn-danger" type="button" value="Delete"></input>
+				                            	</td>  
+			                            	</tr> 
+			                            </tbody>
+		                    </table>	
+	                    </div>
+	                </div>
+	            </div>
+			</div>
+	    </div>
 	</div>
 </body>
 </html>
